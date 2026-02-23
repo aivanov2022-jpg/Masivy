98365a21cc8d189b9ffc4182d16e240192a2fa01
“aivanov2022-jpg” <a.ivanov2022@pmg-pernik.com>
2026-02-23 08:40:08 +0200

    message

--- Program.cs
+++ Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Masivy
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        
+        List<string> array = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+        string command;
+        while ((command = Console.ReadLine()) != "END")
         {
-            Console.WriteLine("Hello, World!");
+            string[] cmdParts = command.Split(' ');
+
+           
+            if (cmdParts[0] == "Reverse")
+            {
+                array.Reverse();
+            }
+            
+            else if (cmdParts[0] == "Distinct")
+            {
+                array = array.Distinct().ToList();
+            }
+           
+            else if (cmdParts[0] == "Replace")
+            {
+                
+               
+
+                int index;
+                bool isNumber = int.TryParse(cmdParts[1], out index);
+
+                
+                if (!isNumber || index < 0 || index >= array.Count)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                string newValue = cmdParts[2];
+                array[index] = newValue;
+            }
+            
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
         }
+
+        
+        Console.WriteLine(string.Join(", ", array));
     }
 }
